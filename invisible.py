a746f071d3cd3fe97c0ab3b82e1de46e8ff40d7f
HOGAN <93939923+HOGANCLAN236@users.noreply.github.com>
2025-05-16 18:24:23 +0100

    Add files via upload

---
+++ invisible.py
@@ -0,0 +1,62 @@
+import  numpy as np
+import cv2
+import time
+
+cap = cv2.VideoCapture(1)
+time.sleep(2)
+background=0
+
+#capture the background
+for i in range(30):
+    ret,background = cap.read()
+
+while(cap.isOpened()):
+    ret , img = cap.read()
+
+    if not ret:
+        break
+
+    hsv = cv2.cvtColor(img , cv2.COLOR_BGR2HSV)
+
+    # lower_red = np.array([0,120,70])
+    # upper_red = np.array([10, 255, 255])
+    # mask1 = cv2.inRange(hsv , lower_red , upper_red) #sepatreting the clock part
+
+    # lower_red = np.array([170, 120, 70])
+    # upper_red = np.array([180, 255, 255])
+    # mask2 = cv2.inRange(hsv, lower_red, upper_red)  # sepatreting the clock part
+
+    #-------------------------------------BLOCK----------------------------# 
+    # setting the lower and upper range for detecting white
+    lower_white1 = np.array([0, 0, 100])
+    upper_white1 = np.array([180, 150, 255])
+    mask1 = cv2.inRange(hsv, lower_white1, upper_white1)
+    #----------------------------------------------------------------------#
+
+    # Second white range (a bit broader to capture variations)
+    lower_white2 = np.array([0, 0, 180])
+    upper_white2 = np.array([180, 50, 255])
+    mask2 = cv2.inRange(hsv, lower_white2, upper_white2)
+    #----------------------------------------------------------------------#
+
+    mask1 =  mask1+mask2 #OR 1 or x
+
+    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN,
+                             np.ones((3,3),np.uint8),iterations=2) #Noise Removal
+    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_DILATE,
+                             np.ones((3,3), np.uint8), iterations=1) #smmoting the image
+
+    mask2 = cv2.bitwise_not(mask1) #Except the clock
+
+    res1=cv2.bitwise_and(background , background , mask = mask1)
+    res2 = cv2.bitwise_and(img , img , mask = mask2)
+
+    final_output = cv2.addWeighted(res1,1,res2,1,0)
+
+    cv2.imshow("Hey invisible..!", final_output)
+    k = cv2.waitKey(10)
+    if k == ord('s'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
